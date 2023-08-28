7cacf7203d540ee7da65c7919fc0dedb5f32a2fe
Ethan <69707914+EthanFrancisco@users.noreply.github.com>
2023-08-28 12:09:04 +0800

    Create Program No. 3

---
+++ Program No3.c
@@ -0,0 +1,22 @@
+#include <ez8.h>
+
+void delay_ms(unsigned int delay);
+
+void main() {
+    PADD = 0x00;
+    PAAF = 0x00;
+    PAOC = 0x00;
+    PAHDE = 0xFF;
+
+    PAOUT = 0xFF;
+
+    while(1) {
+        PAOUT ^= 0xC3; delay_ms(1000);
+    }
+}
+
+void delay_ms(unsigned int delay) {
+    unsigned int x, y;
+    for(x = 0; x <= delay; x++)
+        for(y = 0; y <= 512; y++);
+}
