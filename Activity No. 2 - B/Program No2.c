135a5bb3d355118bdba921d51826f2aa360f63c4
Ethan <69707914+EthanFrancisco@users.noreply.github.com>
2023-09-20 20:30:52 +0800

    Create Program No. 2

--- Activity No. 2 - B/Program No2.c
+++ Activity No. 2 - B/Program No2.c
@@ -1,42 +1,52 @@
 #include <ez8.h>
 #include <stdio.h>
 
-#define sw1 (PBIN & 0x01) // E
+#define sw1 (PBIN & 0x04) // E
 #define sw2 (PBIN & 0x02) // A
-#define sw3 (PBIN & 0x04) // B
+#define sw3 (PBIN & 0x01) // B
 
 void delay(unsigned int ms);
 
 void main() {
-    PADD = 0xF0;
+    PADD = 0x0F;
     PAAF = 0x00;
     PAOC = 0x00;
-    PAHDE = 0xFF;
+    PAHDE = 0xF0;
 
     PBDD = 0x07;
     PBAF = 0x00;
 
     while(1) {
-        if((sw1 != 0) && ((sw2 != 0) || (sw2 == 0)) && ((sw3 != 0) || (sw3 == 0))) {
-            PAOUT = ~0x0F;
+        if((sw1 == 0) && ((sw2 != 0) || (sw2 == 0)) && ((sw3 != 0) || (sw3 == 0))) {
+            PAOUT = ~0xF0;
+            printf("PAOUT = %02X\n", PAOUT);
+            delay(1000);
         }
-        else if((sw1 == 0) && (sw2 == 0) && (sw3 == 0)) {
-            PAOUT = ~0x07;
+        if((sw1 != 0) && (sw2 != 0) && (sw3 != 0)) {
+            PAOUT = ~0x70;
+            printf("PAOUT = %02X\n", PAOUT);
+            delay(1000);
         }
-        else if((sw1 == 0) && (sw2 == 0) && (sw3 != 0)) {
-            PAOUT = ~0x03;
+        else if((sw1 != 0) && (sw2 != 0) && (sw3 == 0)) {
+            PAOUT = ~0xB0;
+            printf("PAOUT = %02X\n", PAOUT);
+            delay(1000);
         }
-        else if((sw1 == 0) && (sw2 != 0) && (sw3 == 0)) {
-            PAOUT = ~0x0C;
+        else if((sw1 != 0) && (sw2 == 0) && (sw3 != 0)) {
+            PAOUT = ~0xD0;
+            printf("PAOUT = %02X\n", PAOUT);
+            delay(1000);
         }
-        else if((sw1 == 0) && (sw2 != 0) && (sw3 != 0)) {
-            PAOUT = ~0x0E;
+        else if((sw1 != 0) && (sw2 == 0) && (sw3 == 0)) {
+            PAOUT = ~0xE0;
+            printf("PAOUT = %02X\n", PAOUT);
+            delay(1000);
         }
     }
 }
 
 void delay(unsigned int ms) {
     unsigned int x, y;
-    for(x = 0; x <= delay; x++)
+    for(x = 0; x <= ms; x++)
         for(y = 0; y <= 512; y++);
 }
