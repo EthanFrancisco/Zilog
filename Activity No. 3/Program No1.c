711ff0430a47056d274ebb60866c84655d4a3b5b
Ethan <69707914+EthanFrancisco@users.noreply.github.com>
2023-10-09 20:40:35 +0800

    Updated Program No. 1

--- Activity No. 3/Program No1.c
+++ Activity No. 3/Program No1.c
@@ -1,9 +1,7 @@
 #include <ez8.h>
 
 #define IN1 (PBIN&0x02)
-#define IN0 (PBIN&0x01)
-
-void delay_ms(unsigned int delay);
+#define IN2 (PBIN&0x01)
 
 void main() {
     PCDD &= 0xF0;
@@ -14,33 +12,27 @@ void main() {
     PBDD |= 0x03;
     PBAF &= ~0x03;
     while(1) {
-        if((IN1 == 0) && (IN0 != 0)) {
+        if((IN1 == 0) && (IN2 != 0)) {
             // Clockwise 7 steps
-            PCOUT = 0x0C; delay_ms(500);
-            PCOUT = 0x06; delay_ms(500);
-            PCOUT = 0x03; delay_ms(500);
-            PCOUT = 0x09; delay_ms(500);
-            PCOUT = 0x0C; delay_ms(500);
-            PCOUT = 0x06; delay_ms(500);
-            PCOUT = 0x03; delay_ms(500);
-        } else if((IN1 != 0) && (IN0 == 0)) {
+            PCOUT |= 0x08; PCOUT &= 0xF8;
+            PCOUT |= 0x04; PCOUT &= 0xF4;
+            PCOUT |= 0x02; PCOUT &= 0xF2;
+            PCOUT |= 0x01; PCOUT &= 0xF1;
+            PCOUT |= 0x08; PCOUT &= 0xF8;
+            PCOUT |= 0x04; PCOUT &= 0xF4;
+            PCOUT |= 0x02; PCOUT &= 0xF2;
+            PCOUT |= 0x01; PCOUT &= 0xF1;
+        } else if((IN1 != 0) && (IN2 == 0)) {
             // C. Clockwise 7 steps
-            PCOUT = 0x09; delay_ms(500);
-            PCOUT = 0x03; delay_ms(500);
-            PCOUT = 0x06; delay_ms(500);
-            PCOUT = 0x0C; delay_ms(500);
-            PCOUT = 0x09; delay_ms(500);
-            PCOUT = 0x03; delay_ms(500);
-            PCOUT = 0x06; delay_ms(500);
-        } else if((IN1 == 0) && (IN0 == 0))
-            PCOUT = 0x00;
-        else if((IN1 != 0) && (IN0 != 0))
-            PCOUT = 0x00;
+            PCOUT |= 0x01; PCOUT &= 0xF1;
+            PCOUT |= 0x02; PCOUT &= 0xF2;
+            PCOUT |= 0x04; PCOUT &= 0xF4;
+            PCOUT |= 0x08; PCOUT &= 0xF8;
+            PCOUT |= 0x01; PCOUT &= 0xF1;
+            PCOUT |= 0x02; PCOUT &= 0xF2;
+            PCOUT |= 0x06; PCOUT &= 0xF4;
+            PCOUT |= 0x08; PCOUT &= 0xF8;
+        } else 
+            PCOUT &= 0xF0;
     }
-}
-
-void delay_ms(unsigned int delay) {
-    unsigned int x, y;
-    for(x = 0; x <= delay; x++)
-        for(y = 0; y <= 512; y++);
 }
