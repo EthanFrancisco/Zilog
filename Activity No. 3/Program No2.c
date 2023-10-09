978ad0133f12a7b5860a3cbb2a005da3c8bef38b
Ethan <69707914+EthanFrancisco@users.noreply.github.com>
2023-10-09 20:44:25 +0800

    Updated Program No. 2

--- Activity No. 3/Program No2.c
+++ Activity No. 3/Program No2.c
@@ -1,7 +1,7 @@
 #include <ez8.h>
 
 #define IN1 (PBIN&0x02)
-#define IN0 (PBIN&0x01)
+#define IN2 (PBIN&0x04)
 
 void main() {
     PADD &= 0x0F;
@@ -9,16 +9,21 @@ void main() {
     PAOC &= 0x0F;
     PAHDE |= 0xF0;
 
-    PBDD |= 0x03;
-    PBAF &= ~0x03;
+    PBDD |= 0x06;
+    PBAF &= ~0x06;
     while(1) {
-        if((IN1 == 0) && (IN0 != 0))
-            PAOUT = 0x10;
-        else if((IN1 != 0) && (IN0 == 0))
-            PAOUT = 0x80;
-        else if((IN1 == 0) && (IN0 == 0))
-            PAOUT = 0x90;
-        else if((IN1 != 0) && (IN0 != 0))
-            PAOUT = 0x60;
+        if((IN1 == 0) && (IN2 != 0)) {
+            PAOUT |= 0x10;
+            PAOUT &= 0x1F;
+        } else if((IN1 != 0) && (IN2 == 0)) {
+            PAOUT |= 0x80;
+            PAOUT &= 0x8F;
+        } else if((IN1 == 0) && (IN2 == 0)) {
+            PAOUT |= 0x90;
+            PAOUT &= 0x9F;
+        } else {
+            PAOUT |= 0x60;
+            PAOUT &= 0x6F;
+        }
     }
 }
