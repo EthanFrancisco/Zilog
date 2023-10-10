d6b16d51932609e381319ed515e7ff1be78f95a5
Ethan <69707914+EthanFrancisco@users.noreply.github.com>
2023-10-11 07:52:31 +0800

    Update Exercise No. 3

--- Experiment No. 2/Exercise No3.c
+++ Experiment No. 2/Exercise No3.c
@@ -4,16 +4,14 @@
 
 int userInput;
 
-void delay_ms(unsigned int delay);
-
 void main() {
     init_uart(_UART0, _DEFFREQ, _DEFBAUD);
     select_port(_UART0);
 
     // Changed to Port C from Port A (Z8F0822 Compatibility)
-    PCDD &= ~0x0F;
-    PCAF &= ~0x0F;
-    PCOC &= ~0x0F;
+    PCDD &= 0xF0;
+    PCAF &= 0xF0;
+    PCOC &= 0xF0;
     PCHDE |= 0x0F;
 
     PADD |= 0x30;
@@ -24,58 +22,55 @@ void main() {
         switch(userInput) {
             case 1:
                 // Clockwise 12 Steps (1-coil excitation)
-                PCOUT = 0x01; delay_ms(500);
-                PCOUT = 0x02; delay_ms(500);
-                PCOUT = 0x04; delay_ms(500);
-                PCOUT = 0x08; delay_ms(500);
-                PCOUT = 0x01; delay_ms(500);
-                PCOUT = 0x02; delay_ms(500);
-                PCOUT = 0x04; delay_ms(500);
-                PCOUT = 0x08; delay_ms(500);
-                PCOUT = 0x01; delay_ms(500);
-                PCOUT = 0x02; delay_ms(500);
-                PCOUT = 0x04; delay_ms(500);
-                PCOUT = 0x08; delay_ms(500);
+                PCOUT |= 0x01;  PCOUT &= 0xF1;
+                PCOUT |= 0x02;  PCOUT &= 0xF2;
+                PCOUT |= 0x04;  PCOUT &= 0xF4;
+                PCOUT |= 0x08;  PCOUT &= 0xF8;
+                PCOUT |= 0x01;  PCOUT &= 0xF1;
+                PCOUT |= 0x02;  PCOUT &= 0xF2;
+                PCOUT |= 0x04;  PCOUT &= 0xF4;
+                PCOUT |= 0x08;  PCOUT &= 0xF8;
+                PCOUT |= 0x01;  PCOUT &= 0xF1;
+                PCOUT |= 0x02;  PCOUT &= 0xF2;
+                PCOUT |= 0x04;  PCOUT &= 0xF4;
+                PCOUT |= 0x08;  PCOUT &= 0xF8;
+                PCOUT |= 0x01;  PCOUT &= 0xF1;
                 break;
             case 2:
                 // C. Clockwise 9 Steps (2-coil excitation)
-                PCOUT = 0x09; delay_ms(500);
-                PCOUT = 0x0C; delay_ms(500);
-                PCOUT = 0x06; delay_ms(500);
-                PCOUT = 0x03; delay_ms(500);
-                PCOUT = 0x09; delay_ms(500);
-                PCOUT = 0x0C; delay_ms(500);
-                PCOUT = 0x03; delay_ms(500);
-                PCOUT = 0x09; delay_ms(500);
-                PCOUT = 0x0C; delay_ms(500);
+                PCOUT |= 0x09;  PCOUT &= 0xF9;
+                PCOUT |= 0x0C;  PCOUT &= 0xFC;
+                PCOUT |= 0x06;  PCOUT &= 0xF6;
+                PCOUT |= 0x03;  PCOUT &= 0xF3;
+                PCOUT |= 0x09;  PCOUT &= 0xF9;
+                PCOUT |= 0x0C;  PCOUT &= 0xFC;
+                PCOUT |= 0x03;  PCOUT &= 0xF3;
+                PCOUT |= 0x09;  PCOUT &= 0xF9;
+                PCOUT |= 0x0C;  PCOUT &= 0xFC;
+                PCOUT |= 0x06;  PCOUT &= 0xF6;
                 break;
             case 3:
                 // C. Clockwise 15 Steps (half step)
-                PCOUT = 0x01; delay_ms(500);
-                PCOUT = 0x03; delay_ms(500);
-                PCOUT = 0x02; delay_ms(500);
-                PCOUT = 0x06; delay_ms(500);
-                PCOUT = 0x04; delay_ms(500);
-                PCOUT = 0x0C; delay_ms(500);
-                PCOUT = 0x08; delay_ms(500);
-                PCOUT = 0x09; delay_ms(500);
-                PCOUT = 0x01; delay_ms(500);
-                PCOUT = 0x03; delay_ms(500);
-                PCOUT = 0x02; delay_ms(500);
-                PCOUT = 0x06; delay_ms(500);
-                PCOUT = 0x04; delay_ms(500);
-                PCOUT = 0x0C; delay_ms(500);
-                PCOUT = 0x08; delay_ms(500);
+                PCOUT |= 0x01;  PCOUT &= 0xF1;
+                PCOUT |= 0x03;  PCOUT &= 0xF3;
+                PCOUT |= 0x02;  PCOUT &= 0xF2;
+                PCOUT |= 0x06;  PCOUT &= 0xF6;
+                PCOUT |= 0x04;  PCOUT &= 0xF4;
+                PCOUT |= 0x0C;  PCOUT &= 0xFC;
+                PCOUT |= 0x08;  PCOUT &= 0xF8;
+                PCOUT |= 0x09;  PCOUT &= 0xF9;
+                PCOUT |= 0x01;  PCOUT &= 0xF1;
+                PCOUT |= 0x03;  PCOUT &= 0xF3;
+                PCOUT |= 0x02;  PCOUT &= 0xF2;
+                PCOUT |= 0x06;  PCOUT &= 0xF6;
+                PCOUT |= 0x04;  PCOUT &= 0xF4;
+                PCOUT |= 0x0C;  PCOUT &= 0xFC;
+                PCOUT |= 0x08;  PCOUT &= 0xF8;
+                PCOUT |= 0x09;  PCOUT &= 0xF9;
                 break;
             default:
                 // Stop
                 break;
         }
     }
-}
-
-void delay_ms(unsigned int delay) {
-    unsigned int x, y;
-    for(x = 0; x <= delay; x++)
-        for(y = 0; y <= 512; y++);
 }
