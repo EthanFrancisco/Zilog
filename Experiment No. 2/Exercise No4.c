9ffd409c964be2cdb4c86addbb7a13386d712ae5
Ethan <69707914+EthanFrancisco@users.noreply.github.com>
2023-10-11 07:42:22 +0800

    Update Exercise No. 4

--- Experiment No. 2/Exercise No4.c
+++ Experiment No. 2/Exercise No4.c
@@ -9,9 +9,9 @@ void main() {
     select_port(_UART0);
 
     // Changed to Port C from Port A (Z8F0822 Compatibility)
-    PCDD &= 0x0F;
-    PCAF &= 0x0F;
-    PCOC &= 0x0F;
+    PCDD &= 0xF0;
+    PCAF &= 0xF0;
+    PCOC &= 0xF0;
     PCHDE |= 0x0F;
 
     PADD |= 0x30;
@@ -22,23 +22,23 @@ void main() {
         switch(userInput) {
             case W:
                 // DC Motor 2 (Clockwise), DC Motor 1 (Clockwise), Forward
-                PCOUT |= 0x05;
+                PCOUT |= 0x05;  PCOUT &= 0xF5;
                 break;
             case A:
                 // DC Motor 2 (Stop), DC Motor 1 (Clockwise), Turn Left
-                PCOUT |= 0x01;
+                PCOUT |= 0x01;  PCOUT &= 0xF1;
                 break;
             case D:
                 // DC Motor 2 (Clockwise), DC Motor 1 (Stop), Turn Right
-                PCOUT |= 0x04;
+                PCOUT |= 0x04;  PCOUT &= 0xF4;
                 break;
             case S:
                 // DC Motor 2 (C. Clockwise), DC Motor 1 (C. Clockwise), Reverse
-                PCOUT |= 0x0A;
+                PCOUT |= 0x0A;  PCOUT &= 0xFA;
                 break;
             default:
                 // DC Motor 2 (Stop), DC Motor 1 (Stop), Stop
-                PCOUT |= 0x0F
+                PCOUT |= 0x0F;  PCOUT &= 0xFF;
                 break;
         }
     }
