09c77b3d988c6984db33a682260fc3228a57fad2
Ethan <69707914+EthanFrancisco@users.noreply.github.com>
2023-10-16 11:27:32 +0800

    Update Program No. 1

--- Activity No. 4 - Part 2/Program No1.c
+++ Activity No. 4 - Part 2/Program No1.c
@@ -1,10 +1,11 @@
 #include <ez8.h>
 #include <sio.h>
 #include <stdio.h>
+#include <string.h>
 
-char Letter;
+unsigned int MessageIndex = 0, MSG_LEN = 50;
+char Message[50];
 
-void initSW(void);
 void Init_UART0(void);
 
 void main() {
@@ -13,20 +14,19 @@ void main() {
     PAOC = 0x00;
     PAHDE = 0xFF;
 
-    PAOUT = ~0x55;
     init_uart(_UART0, _DEFFREQ, _DEFBAUD);
     select_port(_UART0);
     Init_UART0();
     while(1) {
-        if(Letter == 'A') {
-            PAOUT &= 0xEC; 
-        } else if(Letter == 'E') {
+        if(strstr(Message, "A") != 0) {
+            PAOUT &= 0xEC;
+        } else if(strstr(Message, "E") != 0) {
             PAOUT |= 0x2C;
-        } else if(Letter == 'I') {
+        } else if(strstr(Message, "I") != 0) {
             PAOUT ^= 0x52;
-        } else if(Letter == 'O') {
+        } else if(strstr(Message, "O") != 0) {
             PAOUT <<= 3;
-        } else if(Letter == 'U') {
+        } else if(strstr(Message, "U") != 0) {
             PAOUT >>= 4;
         }
     }
@@ -35,14 +35,14 @@ void main() {
 #pragma interrupt
 void Read_UART0(void) {
     if(U0STAT0&0x80)
-        Letter = U0RXD;
+        Message[MessageIndex++] = U0RXD;
 }
 
 void Init_UART0(void) {
     DI();
     SET_VECTOR(UART0_RX, Read_UART0);
-    IRQ0ENL = 0x00;
     IRQ0ENH = 0x10;
+    IRQ0ENL = 0x00;
     IRQ0 &= ~0x10;
     EI();
 }
