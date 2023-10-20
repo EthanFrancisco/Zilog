922e21896612f18a6d912d89ae7c8c39658ad078
Ethan <69707914+EthanFrancisco@users.noreply.github.com>
2023-10-21 00:18:28 +0800

    Create Program No. 2

---
+++ Hardware Timer and Timer Interrupt/Program No2.c
@@ -0,0 +1,33 @@
+#include <ez8.h>
+#include <stdio.h>
+#include <sio.h>
+
+#define PRESCALER
+#define TMODE
+#define RELOAD
+#define PWM
+
+void InitPort(void);
+void Init_Timer(void);
+
+void main() {
+    InitPort();
+    Init_Timer();
+}
+
+void InitPort(void) {
+    PAAF |= 0x02;
+    PAHDE |= 0x02;
+}
+
+void Init_Timer(void) {
+    T0CTL = 0x00;
+    T0CTL = (PRESCALER << 3) | TMODE;
+    T0H = 0x00;
+    T0L = 0x01;
+    T0RH = RELOAD >> 8;
+    T0RL = RELOAD&0x00FF;
+    T0PWMH = PWM >> 8;
+    T0PWM = PWM&0x00FF;
+    T0CTL |= 0x80;
+}
